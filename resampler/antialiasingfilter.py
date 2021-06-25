fbc20f2cafc622daff67fbf8d9e1ab1d7cfed9e5
Hans Baier <hansfbaier@gmail.com>
2021-06-25 20:03:17 +0700

    Heureka! IIR-Filter works!

--- resampler/antialiasingfilter.py
+++ resampler/antialiasingfilter.py
@@ -13,27 +13,27 @@ class AntialiasingFilter(Elaboratable):
         self.audio_out = Signal(signed(bitwidth))
         self.bitwidth = bitwidth
 
-        filter_order = 6
+        filter_order = 5
         num_coefficients = filter_order + 1
         nyquist_frequency = samplerate * 0.5
         cutoff = cutoff_freq / nyquist_frequency
         allowed_ripple = 1.0 # dB
         b, a = signal.cheby1(filter_order, allowed_ripple, cutoff, btype='lowpass', output='ba')
 
-        # convert to fixed point representation (40 fractional bits)
-        self.fraction_bits = fraction_bits = 40
+        # convert to fixed point representation
+        self.fraction_bits = fraction_bits = 24
         assert bitwidth < fraction_bits, f"Bitwidth must not exceed {fraction_bits}"
         self.b = b_fp = [int(x * 2**fraction_bits) for x in b]
         self.a = a_fp = [int(x * 2**fraction_bits) for x in a]
         print(f"{filter_order}-order Chebyshev-Filter cutoff: {cutoff * nyquist_frequency} max ripple: {allowed_ripple}dB\n")
-        print(f"b: {pformat(b, width=160)}")
-        print(f"a: {pformat(a, width=160)}")
-        print(f"b ({pformat(bitwidth, width=160)}.40 fixed point): {b_fp}")
-        print(f"a ({pformat(bitwidth, width=160)}.40 fixed point): {a_fp}\n")
+        print(f"b: {pformat(b)}")
+        print(f"a: {pformat(a)}")
+        print(f"b ({bitwidth}.{fraction_bits} fixed point): {b_fp}")
+        print(f"a ({bitwidth}.{fraction_bits} fixed point): {a_fp}\n")
         assert len(b_fp) == len(a_fp)
 
         def conversion_error(coeff, fp_coeff):
-            val = 2**bitwidth - 1
+            val = 2**(bitwidth - 1)
             fp_product = fp_coeff * val
             fp_result = fp_product >> fraction_bits
             fp_error = fp_result - (coeff * val)
@@ -57,14 +57,24 @@ class AntialiasingFilter(Elaboratable):
         # b are the input coefficients
         # a are the recursive (output) coefficients
         n = len(self.a)
-        b = [Const((1 << 40)) for _ in range(n)]
-        a = [Const(1 << 40) for _ in range(n - 1)]
-        x = Array(Signal(signed(self.bitwidth + self.fraction_bits), name=f"x{i}") for i in range(n))
-        y = Array(Signal(signed(self.bitwidth + self.fraction_bits), name=f"y{i+1}") for i in range(n - 1))
+        width = self.bitwidth + self.fraction_bits
+        b = [Const(n, signed(width)) for n in self.b]
+        # the filter design tool generates a '1.0' coefficient for a_n, which we don't need
+        a = [Const(n, signed(width)) for n in self.a[1:]]
+
+        # we use the array indices flipped, ascending from zero
+        # so x[0] is x_n, x[1] is x_n-1, x[2] is x_n-2 ...
+        # in other words: higher indices are past values, 0 is most recent
+        x = Array(Signal(signed(width), name=f"x{i}") for i in range(n))
+        # because y[0] would be the output value, the y array is shifted by one:
+        # y[0] is y_n-1, y[1] is y_n-2, y[2] is y_n-3
+        # but the signals are still named 'right' to be easy to understnad
+        # in the waveform viewer
+        y = Array(Signal(signed(width), name=f"y{i+1}") for i in range(n - 1))
 
         m.d.comb += self.audio_out.eq(
               sum([((x[i] * b[i]) >> self.fraction_bits) for i in range(n)])
-            + sum([((y[i] * a[i]) >> self.fraction_bits) for i in range(n - 1)]))
+            - sum([((y[i] * a[i]) >> self.fraction_bits) for i in range(n - 1)]))
 
         m.d.sync += [x[i + 1].eq(x[i]) for i in range(n - 1)]
         m.d.sync += [y[i + 1].eq(y[i]) for i in range(n - 2)]
