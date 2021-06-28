c938d59023d252b9252e3a9df30977e7ae99c313
Hans Baier <hansfbaier@gmail.com>
2021-06-28 11:56:32 +0700

    resampler.py: first implementation works

--- resampler/resampler.py
+++ resampler/resampler.py
@@ -2,21 +2,90 @@
 #
 # Copyright (c) 2021 Hans Baier <hansfbaier@gmail.com>
 # SPDX-License-Identifier: CERN-OHL-W-2.0
+
+from resampler.fixedpointiirfilter import FixedPointIIRFilter
 from nmigen import *
+from nmigen.lib.fifo import SyncFIFO
+from nmigen_library.stream import StreamInterface
 
-class AntialiasingFilter(Elaboratable):
-    def __init__(self) -> None:
-        self.audio_in = Signal(signed(16))
-        self.audio_out = Signal(signed(16))
+class FractionalResampler(Elaboratable):
+    def __init__(self, *, input_samplerate: int,
+                 upsample_factor: int, downsample_factor: int,
+                 filter_cutoff: int = 20000,
+                 bitwidth: int = 16) -> None:
+        self.signal_in  = StreamInterface(payload_width=bitwidth)
+        self.signal_out = StreamInterface(payload_width=bitwidth)
 
-    def elaborate(self, platform):
+        self.input_samplerate = input_samplerate
+        self.upsample_factor = upsample_factor
+        self.downsample_factor = downsample_factor
+        self.filter_cutoff = filter_cutoff
+        self.bitwidth = bitwidth
+
+    def elaborate(self, platform) -> Module:
         m = Module()
 
-        # the recursive coefficiens
-        a_f40 = [2588524, 15531147, 38827867, 51770490, 38827867, 15531147, 2588524]
+        m.submodules.antialiasingfilter = antialiasingfilter = \
+            FixedPointIIRFilter(self.input_samplerate * self.upsample_factor,
+                                bitwidth=self.bitwidth,
+                                cutoff_freq=self.filter_cutoff,
+                                filter_order=3)
 
-        m.d.sync += [
-            self.p.eq(self.a * self.b)
+        m.submodules.downsamplefifo = downsamplefifo = \
+            SyncFIFO(width=self.bitwidth, depth=self.upsample_factor)
+
+        # upsampling
+        upsampled_signal  = Signal.like(self.signal_in.payload)
+        upsample_counter  = Signal(self.upsample_factor)
+        input_data        = Signal.like(self.signal_in.payload)
+        input_ready       = Signal()
+        input_valid       = Signal()
+
+        m.d.comb += [
+            self.signal_in.ready.eq(input_ready),
+            input_valid.eq(self.signal_in.valid),
+            input_ready.eq((upsample_counter == 0) & (downsamplefifo.w_rdy)),
+            input_data.eq(self.signal_in.payload),
+            antialiasingfilter.signal_in.eq(upsampled_signal),
+            antialiasingfilter.enable_in.eq(upsample_counter > 0),
+            downsamplefifo.w_en.eq(downsamplefifo.w_rdy & antialiasingfilter.enable_in),
         ]
+
+        with m.If(input_valid & input_ready):
+            m.d.comb += [ 
+                upsampled_signal.eq(self.signal_in.payload),
+                antialiasingfilter.enable_in.eq(1),
+            ]
+            m.d.sync += upsample_counter.eq(self.upsample_factor - 1)
+        with m.Elif(upsample_counter > 0):
+            m.d.comb += upsampled_signal.eq(0)
+            m.d.sync += upsample_counter.eq(upsample_counter - 1)
+
+        # downsampling and output
+        downsample_counter = Signal(self.downsample_factor)
+
+        m.d.comb += [
+            downsamplefifo.w_data.eq(antialiasingfilter.signal_out),
+            self.signal_out.valid.eq(downsamplefifo.r_rdy & downsamplefifo.r_en),
+        ]
+
+        with m.If(downsamplefifo.r_rdy & self.signal_out.ready):
+            m.d.comb += downsamplefifo.r_en.eq(1)
+
+            with m.If(downsample_counter == 0):
+                m.d.sync += downsample_counter.eq(self.downsample_factor - 1)
+                m.d.comb += [
+                    self.signal_out.payload.eq(downsamplefifo.r_data),
+                    self.signal_out.valid.eq(1),
+                ]
+            with m.Else():
+                m.d.sync += downsample_counter.eq(downsample_counter - 1)
+                m.d.comb += self.signal_out.valid.eq(0)
+
+        with m.Else():
+            m.d.comb += [
+                downsamplefifo.r_en.eq(0),
+                self.signal_out.valid.eq(0),
+            ]
 
         return m
---
+++ test/resampler-bench.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2021 Hans Baier <hansfbaier@gmail.com>
+# SPDX-License-Identifier: MIT
+
+import sys
+sys.path.append('.')
+
+from nmigen.sim import Simulator, Tick
+from resampler.resampler import FractionalResampler
+
+if __name__ == "__main__":
+    dut = FractionalResampler(input_samplerate=56000, upsample_factor=6, downsample_factor=7, filter_cutoff=20000)
+    sim = Simulator(dut)
+
+    def sync_process():
+        for _ in range(10): yield Tick()
+        yield dut.signal_out.ready.eq(1)
+        for i in range(1000):
+            yield Tick()
+            if i < 400:
+                if i % 6 == 0:
+                    yield dut.signal_in.valid.eq(1)
+                    yield dut.signal_in.payload.eq(2**14)
+                else:
+                    yield dut.signal_in.valid.eq(0)
+            else:
+                if i % 6 == 0:
+                    yield dut.signal_in.valid.eq(1)
+                    yield dut.signal_in.payload.eq(-2**14)
+                else:
+                    yield dut.signal_in.valid.eq(0)
+
+    sim.add_sync_process(sync_process)
+    sim.add_clock(10e-9)
+    with sim.write_vcd('resampler.vcd', traces=[dut.signal_in.payload, dut.signal_in.valid, dut.signal_out.valid, dut.signal_out.payload]):
+        sim.run()
