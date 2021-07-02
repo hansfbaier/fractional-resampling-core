582edfb5b734343f80ddd486128bee053c69942b
Hans Baier <hansfbaier@gmail.com>
2021-07-02 10:31:35 +0700

    add FixedPointFIRFilter

--- resampler/__init__.py
+++ resampler/__init__.py
@@ -1,5 +1,6 @@
 from .resampler           import FractionalResampler
 from .fixedpointiirfilter import FixedPointIIRFilter
+from .fixedpointfirfilter import FixedPointFIRFilter
 from .filterbank          import Filterbank
 
-__all__ = [FractionalResampler, FixedPointIIRFilter, Filterbank]
+__all__ = [FractionalResampler, FixedPointIIRFilter, FixedPointFIRFilter, Filterbank]
---
+++ test/fixedpointfirfilter-bench.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2021 Hans Baier <hansfbaier@gmail.com>
+# SPDX-License-Identifier: MIT
+
+import sys
+sys.path.append('.')
+
+from nmigen.sim import Simulator, Tick
+from resampler.fixedpointfirfilter import FixedPointFIRFilter
+
+if __name__ == "__main__":
+    dut = FixedPointFIRFilter(336000)
+    sim = Simulator(dut)
+
+    def sync_process():
+        max = int(2**15 - 1)
+        min = -max
+        yield dut.enable_in.eq(1)
+        for _ in range(20): yield Tick()
+        yield dut.signal_in.eq(max)
+        for _ in range(100): yield Tick()
+        yield dut.signal_in.eq(min)
+        for _ in range(5): yield Tick()
+        yield dut.enable_in.eq(0)
+        for _ in range(20): yield Tick()
+        yield dut.enable_in.eq(1)
+        for _ in range(60): yield Tick()
+        yield dut.signal_in.eq(0)
+        for _ in range(100): yield Tick()
+        for i in range(10):
+           yield dut.signal_in.eq(max)
+           yield Tick()
+           yield dut.signal_in.eq(0)
+           yield Tick()
+           for _ in range(6): yield Tick()
+           yield dut.signal_in.eq(min)
+           yield Tick()
+           yield dut.signal_in.eq(0)
+           yield Tick()
+           for _ in range(6): yield Tick()
+
+    sim.add_sync_process(sync_process)
+    sim.add_clock(10e-9)
+    with sim.write_vcd('fixedpointfirfilter.vcd', traces=[dut.signal_in, dut.signal_out]):
+        sim.run()
